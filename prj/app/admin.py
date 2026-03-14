7a105cd66a73fa5624a5995825f3f7d382446cb9
MarekPlasek <marek.plasek.s@gyarab.cz>
2026-03-14 18:21:45 +0100

    sync commit

--- prj/app/admin.py
+++ prj/app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Album, Artist
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+	pass
+
+@admin.register(Artist)
+class ArtistAdmin(admin.ModelAdmin):
+	pass
--- prj/app/models.py
+++ prj/app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Album(models.Model):
+	title = models.CharField(max_length=255)
+	artist = models.ForeignKey('Artist', on_delete=models.SET_NULL, null=True)
+	producer = models.CharField(max_length=255)
+	release_date = models.DateField()
+	mb_id = models.PositiveSmallIntegerField()
+	discogs_url = models.CharField()
+	lastfm_url = models.CharField()
+
+class Artist(models.Model):
+	name = models.CharField(max_length=255)
+	mb_id = models.PositiveSmallIntegerField()
+	discogs_url = models.CharField()
+	lastfm_url = models.CharField()
+	
