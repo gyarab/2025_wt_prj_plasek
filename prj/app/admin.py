00b4fb723e57bc143dedd37330b668a134745bcd
MarekPlasek <marek.plasek.s@gyarab.cz>
2026-03-26 23:14:18 +0100

    added models 'n' stuff

--- prj/app/admin.py
+++ prj/app/admin.py
@@ -1,12 +1,28 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Album, Artist
+from .models import Album, Track, Artist, Producer, Genre, Release
 
 @admin.register(Album)
 class AlbumAdmin(admin.ModelAdmin):
 	pass
 
+@admin.register(Track)
+class TrackAdmin(admin.ModelAdmin):
+	pass
+
 @admin.register(Artist)
 class ArtistAdmin(admin.ModelAdmin):
+	pass
+
+@admin.register(Producer)
+class ProducerAdmin(admin.ModelAdmin):
+	pass
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+	pass
+
+@admin.register(Release)
+class ReleaseAdmin(admin.ModelAdmin):
 	pass
--- prj/app/models.py
+++ prj/app/models.py
@@ -6,13 +6,40 @@ class Album(models.Model):
 	artist = models.ForeignKey('Artist', on_delete=models.SET_NULL, null=True)
 	producer = models.CharField(max_length=255)
 	release_date = models.DateField()
+	genre = models.ForeignKey('Genre', on_delete=models.SET_NULL, null=True)
 	mb_id = models.PositiveSmallIntegerField()
 	discogs_url = models.CharField()
 	lastfm_url = models.CharField()
+
+class Track(models.Model):
+	title = models.CharField(max_length=255)
+	artist = models.ForeignKey('Artist', on_delete=models.SET_NULL, null=True)
+	producer = models.ForeignKey('Producer', on_delete=models.SET_NULL, null=True)
+	release_date = models.DateField()
+	genre = models.ForeignKey('Genre', on_delete=models.SET_NULL, null=True)
+	mb_id = models.PositiveSmallIntegerField()
+	album = models.ForeignKey('Album', on_delete=models.SET_NULL, null=True)
 
 class Artist(models.Model):
 	name = models.CharField(max_length=255)
 	mb_id = models.PositiveSmallIntegerField()
 	discogs_url = models.CharField()
 	lastfm_url = models.CharField()
-	
+
+class Producer(models.Model):
+	name = models.CharField(max_length=255)
+	mb_id = models.PositiveSmallIntegerField()
+	discogs_url = models.CharField()
+	lastfm_url = models.CharField()
+
+class Genre(models.Model):
+	name = models.CharField(max_length=255)
+	description = models.CharField(max_length=255)	
+
+class Release(models.Model):
+	name = models.CharField(max_length=255)
+	release_media = models.CharField(max_length=255)
+	album = models.ForeignKey('Album', on_delete=models.SET_NULL, null=True)
+	single = models.ForeignKey('Track', on_delete=models.SET_NULL, null=True)
+	discogs_url = models.CharField()
+	lastfm_url = models.CharField()
